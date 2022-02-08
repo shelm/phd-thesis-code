23de4d74e2787d4371f326c22561c3498bfc9ba7
Dominik Schiller <dominik.schiller@me.com>
2022-02-08 08:40:10 +0100

    Add initial emonet prediction pipeline

--- 02_predict/kassel/predict_emonet.py
+++ 02_predict/kassel/predict_emonet.py
@@ -1,81 +1,90 @@
-import time
-import torch
-
-from hcai_dataset_utils.pytorch_dataset_wrapper import PyTorchDatasetWrapper
-from hcai_models.expansion.preprocessing.blaze_face_detector import BlazeFaceDetector
+import pandas as pd
 from hcai_models.expansion.preprocessing.bounding_box_image_crop import (
     BoundingBoxImageCrop,
 )
 from hcai_models.expansion.preprocessing.s3fd_detector import S3FDDetector
-
+from hcai_models.expansion.preprocessing.blaze_face_detector import BlazeFaceDetector
+from hcai_models.models.image.hcai_sfd.sfd import S3FD
 from hcai_models.models.image.hcai_emonet.emonet import EmoNet8
 import tensorflow_datasets as tfds
 import os
-import hcai_datasets
 import tensorflow as tf
-
-from hcai_models.utils.metric_utils import ACC, SAGR, CCC, PCC, RMSE
-from hcai_models.utils.torch_utils import MetricMapping
-
+import hcai_datasets
+from utils.config_utils import read_configs, builder_kwargs_from_config_paths
+import cv2
 from matplotlib import pyplot as plt
+import decord
+from decord import VideoReader
+from decord import cpu, gpu
+from tqdm import tqdm
+import numpy as np
+from pathlib import Path
+from utils.config_utils import read_configs
+import pandas
 
-start = time.time()
-from matplotlib import pyplot as plt
+# Configs
+cfg_path = '../../.configs/dynamic_datasets/kassel_emonet.cfg'
+cfg = read_configs([cfg_path])
+cfg_ds = cfg['DATASET']
 
-# Load Data
-ds, ds_info = tfds.load(
-    "hcai_nova_dynamic",
-    split="dynamic_split",
-    with_info=True,
-    as_supervised=False,
-    data_dir=".",
-    shuffle_files=False,
-    read_config=tfds.ReadConfig(shuffle_seed=1337),
-    builder_kwargs={
-        # Database Config
-        "db_config_path": "../configs/nova/nova_db.cfg",
+# Paths
+dataset_root = Path(cfg_ds.get('nova_data_dir')) / cfg_ds.get('dataset')
+outpath = Path('./emonet_predictions')
 
-        # Dataset Config
-        "dataset": "kassel_therapie_korpus",
-        "nova_data_dir": os.path.join(r"/Volumes/Corpora_T5/"),
-        "sessions": ["OPD_101_No"],
-        "roles": ["patient", "therapist"],
-        "schemes": [],
-        "annotator": "system",
-        "data_streams": ["video_resized"],
+# Files
+file_list = []
+for vid in dataset_root.rglob('*.video_resized.mp4'):
+    file_list.append(vid)
 
-        # Sample Config
-        "frame_size": 0.04,
-        "left_context": 0,
-        "right_context": 0,
-        "flatten_samples": False,
+# Preprocessing modules
+face_detector = BlazeFaceDetector()
+face_detector._device = 'gpu'
+bb_cropper = BoundingBoxImageCrop(output_dim=(256, 256))
 
-        # Additional Config
-        "lazy_loading": False,
-        "clear_cache": False
-    }
-)
-
-# Preprocessing
-def pp(batch):
-    image = batch["video_resized"]
-
-    #bb = BlazeFaceDetector()(image)
-    bb = S3FDDetector()(image)
-    image = BoundingBoxImageCrop(output_dim=(256, 256))(image, bb)
-    image = tf.experimental.numpy.moveaxis(image, -1, -3)
-    image = image / 255
-    batch["video_resized"] = image
-    return batch
-
-ds = ds.map(pp)
-data_it = ds.as_numpy_iterator()
-data_list = list(data_it)
-data_list.sort(key=lambda x: int(x["frame"].decode("utf-8").split("_")[0]))
-
-# Build model
-model = EmoNet8(weights="affectnet")
-model.build_model()
+# Emonet model
+emonet = EmoNet8(weights="affectnet")
+emonet.build_model()
+emonet._model.eval()
 
 # Predict
-a = model.predict(data_list['video_resized'])
+for file_path in file_list:
+    csv_out = outpath / ( str(file_path.parent.stem) + '-' + str(file_path.stem) + '.csv')
+    if csv_out.is_file():
+        continue
+    data = {
+        'frame': [],
+        'bb': [],
+        'expression': [],
+        'valence': [],
+        'arousal': [],
+        'heatmap': []
+    }
+    with open(file_path, 'rb') as f:
+        print(f'Predicting {file_path}')
+        vr = VideoReader(f, ctx=cpu(0))
+        for i in tqdm(range(len(vr))):
+            img = vr[i].asnumpy()
+
+            # Detect faces
+            img_pp, (xshift, yshift), orig_size = face_detector.blaze_face.preprocessing(img)
+            bb = face_detector.blaze_face.predict(np.expand_dims(img_pp, 0))
+            detected_faces = face_detector.blaze_face.postprocessing(bb, shifts=(xshift, yshift), orig_size=orig_size)
+
+            # Predict Emotions
+            if detected_faces.any():
+                img_emo = bb_cropper._internal_call(img, np.squeeze(detected_faces[0,:4]))
+                img_emo = img_emo.astype(dtype=np.float32)
+                img_emo = img_emo/255
+                img_emo = np.moveaxis(img_emo, -1, -3)
+                out = emonet.predict(img_emo)
+                data['frame'].append(i)
+                data['bb'].append(detected_faces[0])
+                data['expression'].append(np.squeeze(out['expression'].cpu().numpy()))
+                data['valence'].append(np.squeeze(out['valence'].cpu().numpy()))
+                data['arousal'].append(np.squeeze(out['arousal'].cpu().numpy()))
+                data['heatmap'].append(np.squeeze(out['heatmap'].cpu().numpy()))
+
+    outpath.mkdir(exist_ok=True)
+    df = pd.DataFrame.from_dict(data)
+    df.to_csv(csv_out)
+
